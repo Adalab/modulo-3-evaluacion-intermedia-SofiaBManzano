aa2f8a117f1f473a804e9165ef2e4be634abff3c
SofiaBManzano <sofiabmanzano@gmail.com>
2022-01-18 10:20:32 +0100

    add new adalaber

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,49 @@ import adalabersList from "../services/apijson.json";
 //import callToApi from '../services/api';
 function App() {
   //estados
-  const [adalabers, setAdalabers] = useState(adalabersList);
-
+  const [adalabers, setAdalabers] = useState(adalabersList.results);
+  const [inputName, setInputName] = useState("");
+  const [inputSpeciality, setInputSpeciality] = useState("");
+  const [inputCounselor, setInputCounselor] = useState("");
   //constantes normales
   const title = "Adalabers";
   const getHtml = () => {
-    return adalabersList.results.map((eachAdalaber) => (
-      <tr>
+    return adalabers.map((eachAdalaber) => (
+      <tr key={eachAdalaber.id}>
         <td>{eachAdalaber.name}</td>
         <td>{eachAdalaber.counselor}</td>
         <td>{eachAdalaber.speciality}</td>
       </tr>
     ));
+  };
+
+  //funciones manejadoras
+  const handleInputName = (ev) => {
+    setInputName(ev.currentTarget.value);
+  };
+  const handleInputCounselor = (ev) => {
+    setInputCounselor(ev.currentTarget.value);
+  };
+  const handleInputSpeciality = (ev) => {
+    setInputSpeciality(ev.currentTarget.value);
+  };
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    const newAdalaber = {
+      name: inputName,
+      counselor: inputCounselor,
+      speciality: inputSpeciality,
+    };
+    //me creo una nueva variable con el spread (plagia contactos antiguos y suma los nuevos)
+
+    let newAdalaberList = [...adalabers, newAdalaber];
+
+    //cambio el valor del estado adalabers
+    setAdalabers(newAdalaberList);
+    //limpio inputs
+    setInputSpeciality("");
+    setInputCounselor("");
+    setInputSpeciality("");
   };
 
   return (
@@ -35,13 +66,36 @@ function App() {
       </table>
       <h2>Añadir una Adalaber</h2>
       <form>
-        <label for="name">Nombre:</label>
-        <input type="text" name="name" placeholder="Nombre..." />
-        <label for="name">Tutora:</label>
-        <input type="text" name="counselor" placeholder="Tutora..." />
-        <label for="name">Especialidad</label>
-        <input type="text" name="speciality" placeholder="Especialidad..." />
-        <input className="new-contact__btn" type="submit" value="Añadir" />
+        <label htmlFor="name">Nombre:</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nombre..."
+          value={inputName}
+          onChange={handleInputName}
+        />
+        <label htmlFor="name">Tutora:</label>
+        <input
+          type="text"
+          name="counselor"
+          placeholder="Tutora..."
+          value={inputCounselor}
+          onChange={handleInputCounselor}
+        />
+        <label htmlFor="name">Especialidad</label>
+        <input
+          type="text"
+          name="speciality"
+          placeholder="Especialidad..."
+          value={inputSpeciality}
+          onChange={handleInputSpeciality}
+        />
+        <input
+          className="new-contact__btn"
+          type="submit"
+          value="Añadir"
+          onClick={handleSubmit}
+        />
       </form>
     </div>
   );
